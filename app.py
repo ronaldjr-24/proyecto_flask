77e4798d73eef8add1644c7f8e77ee4810f30bee
ronaldjr-24 <ronaldosalazar568git config --global user.email ronaldosalazar568@gmail.com>
2025-08-15 18:40:04 -0500

    subir

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask
 
-# Creamos la aplicación
-web = Flask(__name__)
+# Crear instancia de la aplicación
+app = Flask(__name__)
 
 # Ruta de inicio
-@web.route('/')
+@app.route('/')
 def inicio():
     return '¡Bienvenido! Has iniciado correctamente tu proyecto Flask.'
 
 # Ruta con parámetro dinámico
-@web.route('/saludo/<usuario>')
+@app.route('/saludo/<string:usuario>')
 def saludo(usuario):
     return f'Hola {usuario}, me alegra verte aquí en la app.'
 
 # Punto de entrada
 if __name__ == '__main__':
     # Modo debug activo para desarrollo
-    web.run(debug=True)
+    app.run(debug=True)
